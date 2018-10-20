c6c4253174c356525158410a6cf48c7d867639e9
Chris Coleman <chrismco08@gmail.com>
2018-10-20 18:14:40 -0500

    Overall Update to code.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
-import createWeatherStore, {history} from '../../store';
+import React from 'react';
+import createWeatherStore from '../../store';
 import { Provider } from 'react-redux';
 import WeatherContainer from '../weatherContainer';
-import { ConnectedRouter } from 'connected-react-router';
+import spinner from '../../assets/spinner.gif';
 
 
 const store = createWeatherStore();
 
-console.log(store.getState())
+// console.log(store.getState())
 
-class App extends Component {
-    render() {
-        if(store){
-            return (
-                <Provider store={store}>
-                 {/* <ConnectedRouter history={history}> */}
-                    <section id="mainWeatherSection">
-                        <div className="container">
-                        <WeatherContainer />
-                        </div>
-                    </section>
-                    {/* </ConnectedRouter> */}
-                </Provider>
-            )
-        }else{
-            return <p>Loading....</p>
-        }
-   
-    }
-}
+const App = () => (
+        (store) ? (
+            <Provider store={store}>
+            {/* <ConnectedRouter history={history}> */}
+               <section id="mainWeatherSection">
+                   <div className="container">
+                   <WeatherContainer />
+                   </div>
+               </section>
+               {/* </ConnectedRouter> */}
+           </Provider>
+        ) : (
+            <img src={spinner} />
+        )
+)
 export default App;
